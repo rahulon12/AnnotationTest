2dfb0642132bcab1531c149322a0867b5469d06e
Rahul Narayanan <rahulon@icloud.com>
2024-02-13 10:12:35 -0500

    Initial Commit

---
+++ AnnotationTest/AnnotationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnotationTestApp.swift
+//  AnnotationTest
+//
+//  Created by Rahul on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AnnotationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
